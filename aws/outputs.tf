bb85cfa5e001ce767ef79f97b592d6b1c6939842
Sean Clark <seanclark@Seans-MacBook-Pro.local>
2019-03-16 12:01:37 -0600

    outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,5 +1,31 @@
-#---storage/outputs.tf
+#---root/outputs.tf
+
+#---storage/outputs
 
 output "bucket name" {
 	value = "${module.storage.bucketname}"
+}
+
+#---networking outputs
+
+output "Public Subnets" {
+	value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "Subnet IPs" {
+	value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "Public Security Group" {
+	value = "${module.networking.public_sg}"
+}
+
+#---compute outputs
+
+output "Public Instance IDs" {
+	value ="${module.compute.server_id"
+}
+
+output "Public Instance IPs" {
+	value = "${module.compute.server_ip}"
 }
