fba3f6f13e9aa841e44b5d86424c454476a3cf29
clarksm <sean.m.clark7@gmail.com>
2019-03-21 07:16:06 -0600

    Update main.tf

--- aws/networking/main.tf
+++ aws/networking/main.tf
@@ -27,10 +27,11 @@ resource "aws_route_table" "tf_public_rt" {
 	route {
 		cidr_block = "0.0.0.0/0"
 		gateway_id = "${aws_internet_gateway.tf_internet_gateway.id}"
+		
+	}
 
-		tags {
+	tags {
 			Name = "tf_public"
-		}
 	}
 }
 
