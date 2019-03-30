f2939a93c163d3a9370a0185504f66b7d8051bff
clarksm <sean.m.clark7@gmail.com>
2019-03-30 17:13:33 -0600

    Update outputs.tf

--- aws/networking/outputs.tf
+++ aws/networking/outputs.tf
@@ -11,3 +11,15 @@ output "public_sg" {
 output "subnet_ips" {
 	value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
 }
+
+output "prd_public_subnets" {
+	value = "${aws_subnet.tf_prd_public_subnet.*.id}"
+}
+
+output "prd_public_sg" {
+	value = "${aws_security_group.tf_prd_public_sg.id}"	
+}
+
+output "prd_subnet_ips" {
+	value = "${aws_subnet.tf_prd_public_subnet.*.cidr_block}"
+}
