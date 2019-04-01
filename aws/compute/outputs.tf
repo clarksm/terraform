1db845abf81e287e06782d184b2788f9b993e01b
clarksm <sean.m.clark7@gmail.com>
2019-04-01 07:42:09 -0600

    Update outputs.tf

--- aws/compute/outputs.tf
+++ aws/compute/outputs.tf
@@ -6,3 +6,11 @@ output "server_id" {
 output "server_ip" {
 	value = "${join(", ", aws_instance.tf_server.*.public_ip)}"
 }
+
+output "prd_server_id" {
+	value = "${join(", ", aws_instance.tf_prd_server.*.id)}"
+}
+
+output "prd_server_ip" {
+	value = "${join(", ", aws_instance.tf_prd_server.*.public_ip)}"
+}
